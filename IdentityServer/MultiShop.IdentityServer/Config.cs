0dab20cce9c4be6e33a4c3b6f755dc0d47fc4002
hüseyin erdin <erdinh61@gmail.com>
2024-07-26 19:24:11 +0300

    Idendtity service added.(IdentityServer4)

---
+++ IdentityServer/MultiShop.IdentityServer/Config.cs
@@ -0,0 +1,106 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace MultiShop.IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
+        {
+            new ApiResource("ResourceCatalog") {Scopes={"CatalogFullPermission","CatalogReadPermission"}},
+            new ApiResource("ResourceDiscount") {Scopes={"DiscountFullPermission"}},
+            new ApiResource("ResourceOrder") {Scopes={"OrderFullPermission"}},
+        };
+
+        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResources.Email(),
+        };
+
+        public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
+        {
+            new ApiScope("CatalogFullPermission","Full authority for catalog operations"),
+            new ApiScope("CatalogReadPermission","Reading authority for catalog operations"),
+            new ApiScope("DiscountFullPermission","Full authority for discount operations"),
+            new ApiScope("OrderFullPermission","Full authority for order operations"),
+        };
+
+        public static IEnumerable<Client> Clients => new Client[]
+        {
+            new Client
+            {
+                ClientId="MultiShopVisitorId",
+                ClientName="Multi Shop Visitor User",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = {new Secret("multishopsecret".Sha256())},
+                AllowedScopes={"CatalogReadPermission"}
+            },
+            new Client
+            {
+                ClientId="MultiShopManagerId",
+                ClientName="Multi Shop Manager User",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = {new Secret("multishopsecret".Sha256())},
+                AllowedScopes={"CatalogReadPermission", "CatalogFullPermission" }
+            },
+            new Client
+            {
+                ClientId="MultiShopAdminId",
+                ClientName="Multi Shop Admin User",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = {new Secret("multishopsecret".Sha256())},
+                AllowedScopes={
+                    "CatalogReadPermission",
+                    "CatalogFullPermission",
+                    "DiscountFullPermission",
+                    "OrderFullPermission",
+                    IdentityServerConstants.LocalApi.ScopeName,
+                    IdentityServerConstants.StandardScopes.Email,
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                },
+                AccessTokenLifetime = 600
+            }
+        };
+
+
+        //public static IEnumerable<Client> Clients =>
+        //    new Client[]
+        //    {
+        //        // m2m client credentials flow client
+        //        new Client
+        //        {
+        //            ClientId = "m2m.client",
+        //            ClientName = "Client Credentials Client",
+
+        //            AllowedGrantTypes = GrantTypes.ClientCredentials,
+        //            ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+
+        //            AllowedScopes = { "scope1" }
+        //        },
+
+        //        // interactive client using code flow + pkce
+        //        new Client
+        //        {
+        //            ClientId = "interactive",
+        //            ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+        //            AllowedGrantTypes = GrantTypes.Code,
+
+        //            RedirectUris = { "https://localhost:44300/signin-oidc" },
+        //            FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
+        //            PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+
+        //            AllowOfflineAccess = true,
+        //            AllowedScopes = { "openid", "profile", "scope2" }
+        //        },
+        //    };
+    }
+}
---
+++ IdentityServer/MultiShop.IdentityServer/Controllers/RegistersController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServer.Dtos;
+using MultiShop.IdentityServer.Models;
+using System.Threading.Tasks;
+
+namespace MultiShop.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegistersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(UserRegisterDto userRegisterDto)
+        {
+            var user = new ApplicationUser()
+            {
+                UserName = userRegisterDto.Username,
+                Email = userRegisterDto.Email,
+                Name = userRegisterDto.Name,
+                Surname = userRegisterDto.Surname,
+            };
+            var result = await _userManager.CreateAsync(user, userRegisterDto.Password);
+            if (result.Succeeded)
+            {
+                return Ok("User successfully created");
+            }
+            else
+            {
+                return BadRequest("User could not created.");
+            }
+        }
+    }
+}
